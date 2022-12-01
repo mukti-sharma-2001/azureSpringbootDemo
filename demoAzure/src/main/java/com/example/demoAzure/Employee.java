100f8547e45322da9b50d4d20697e3784f9c63ae
muktisharma <mukti.sharma@nagarro.com>
2022-12-01 19:17:17 +0530

    adding code

---
+++ demoAzure/src/main/java/com/example/demoAzure/Employee.java
@@ -0,0 +1,23 @@
+package com.example.demoAzure;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+	@Id
+	private int id;
+	private String name;
+	private String dept;
+	private int salary;
+}
